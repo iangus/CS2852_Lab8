86a1306589e72581b3f0a3619d0c473306647e88
Ian Guswiler <iangus97@gmail.com>
2016-05-09 20:55:58 -0500

    Done.

--- src/LookupTable.java
+++ src/LookupTable.java
@@ -1,36 +1,60 @@
+/*
+ * CS2852 - 041
+ * Spring 2016
+ * Lab 8
+ * Name: Ian Guswiler
+ * Created: 5/3/2016
+ */
+
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * CS2852 - 041
- * Spring 2016
- * Lab
- * Name: Ian Guswiler
- * Created: 5/3/2016
+ * Lookup map that holds entries sortable by K values
+ *
+ * @author Ian Guswiler
+ * @version 5/9/16
+ * @param <K> key
+ * @param <V> value
  */
 public class LookupTable<K extends Comparable<K>,V> implements Map<K,V>{
     private List<Entry<K,V>> entries;
 
+    /**
+     * Constructor that creates a new lookup table with an empty ArrayList for entries.
+     */
     public LookupTable(){
         entries = new ArrayList<>();
     }
 
+    /**
+     * Gives the current size of the lookup table
+     * @return size of the lookup table
+     */
     @Override
     public int size() {
         return entries.size();
     }
 
+    /**
+     * Checks if the table is empty
+     * @return returns true if the table is empty
+     */
     @Override
     public boolean isEmpty() {
         return entries.isEmpty();
     }
 
+    /**
+     * Checks the table for a specified key
+     * @param key key to be searched for
+     * @return returns true if an entry with the specified key is found
+     */
     @Override
     public boolean containsKey(Object key) {
         Entry<K,V> lookFor = new AbstractMap.SimpleEntry<>((K) key,null);
@@ -38,11 +62,21 @@ public class LookupTable<K extends Comparable<K>,V> implements Map<K,V>{
         return result >= 0;
     }
 
+    /**
+     * Not implemented
+     * @param value Ignored
+     * @return Not implemented
+     */
     @Override
     public boolean containsValue(Object value) {
         throw new UnsupportedOperationException("This method has not been implemented.");
     }
 
+    /**
+     * Retrieves the value associated with a specified key
+     * @param key the key to which the value is associated
+     * @return value associated with the specified key
+     */
     @Override
     public V get(Object key) {
         Entry<K,V> lookFor = new AbstractMap.SimpleEntry<>((K) key,null);
@@ -50,6 +84,12 @@ public class LookupTable<K extends Comparable<K>,V> implements Map<K,V>{
         return containsKey(key) ? entries.get(index).getValue() : null;
     }
 
+    /**
+     * Places the specified key and value combo in the table
+     * @param key key to be placed in the table
+     * @param value associated value to be placed in the table
+     * @return previous value that the key was associated with. Returns null if the key was not in the table.
+     */
     @Override
     public V put(K key, V value){
         V previousValue = null;
@@ -64,6 +104,11 @@ public class LookupTable<K extends Comparable<K>,V> implements Map<K,V>{
         return previousValue;
     }
 
+    /**
+     * Removes the entry for a specified key
+     * @param key key to be removed from the table
+     * @return value that was removed with the key. Returns null if the key was not in the table.
+     */
     @Override
     public V remove(Object key) {
         V removed = null;
@@ -75,26 +120,44 @@ public class LookupTable<K extends Comparable<K>,V> implements Map<K,V>{
         return removed;
     }
 
+    /**
+     * Not implemented
+     */
     @Override
     public void putAll(Map m) {
         throw new UnsupportedOperationException("This method has not been implemented.");
     }
 
+    /**
+     * Not implemented
+     */
     @Override
     public void clear() {
         throw new UnsupportedOperationException("This method has not been implemented.");
     }
 
+    /**
+     * Not implemented
+     * @return Ignored
+     */
     @Override
     public Set keySet() {
         throw new UnsupportedOperationException("This method has not been implemented.");
     }
 
+    /**
+     * Not implemented
+     * @return Ignored
+     */
     @Override
     public Collection values() {
         throw new UnsupportedOperationException("This method has not been implemented.");
     }
 
+    /**
+     * Not implemented
+     * @return Ignored
+     */
     @Override
     public Set<Entry<K, V>> entrySet() {
         throw new UnsupportedOperationException("This method has not been implemented.");
--- src/MorseEncoder.java
+++ src/MorseEncoder.java
@@ -1,3 +1,11 @@
+/*
+ * CS2852 - 041
+ * Spring 2016
+ * Lab 8
+ * Name: Ian Guswiler
+ * Created: 5/3/2016
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -5,11 +13,10 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * CS2852 - 041
- * Spring 2016
- * Lab
- * Name: Ian Guswiler
- * Created: 5/3/2016
+ * Loads text files and operates on them using a lookup table then saves the encoded message to a file.
+ *
+ * @author Ian Guswiler
+ * @version 5/9/16
  */
 public class MorseEncoder {
     private static LookupTable table = new LookupTable();
@@ -24,6 +31,10 @@ public class MorseEncoder {
         }
     }
 
+    /**
+     * Main functioning of the program. Takes file inputs through scanners and writes to them with a file writer.
+     * @param args Ignored
+     */
     public static void main(String[] args) {
         loadTable(new File("morsecode.txt"));
         Scanner input = new Scanner(System.in);
